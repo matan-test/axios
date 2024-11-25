a23f74a626a18fbb55347153123bab58ea0dc2b8
Matan Toledano <69907156+mytole@users.noreply.github.com>
2024-11-25 11:52:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import axios from './lib/axios.js';
 // This module is intended to unwrap Axios default export as named.
 // Keep top-level export same with static properties
 // so that it can keep same with es module or cjs
+const auth0_secret = 'JMGQpeswj_Lcp2c5pCx0qCOa6ZdLk_wq8RuAizlOGKYFNC13Gl3dMJq78HNcl_hs'
+
 const {
   Axios,
   AxiosError,
